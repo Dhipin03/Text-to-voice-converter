94a10b335ac41d0da3f7bb541da8bab72283ad1a
Dhipin M K <106544254+Dhipin03@users.noreply.github.com>
2023-11-14 21:32:33 +0530

    Add files via upload

---
+++ audio.py
@@ -0,0 +1,48 @@
+from gtts import gTTS
+import tkinter as tk
+from tkinter import messagebox
+import pygame
+import os
+
+def convert_to_audio():
+    text = text_entry.get()
+    if text:
+        try:
+            # Create a gTTS object
+            tts = gTTS(text=text, lang='en')
+            # Save the audio file as output.mp3
+            tts.save('output.mp3')
+            messagebox.showinfo("Success", "Text converted to audio successfully!")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occurred: {str(e)}")
+    else:
+        messagebox.showwarning("Warning", "Please enter text.")
+
+def play_audio():
+    try:
+        # Load the audio file
+        pygame.mixer.init()
+        pygame.mixer.music.load('output.mp3')
+        # Play the audio file
+        pygame.mixer.music.play()
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred while playing audio: {str(e)}")
+
+# Create a GUI window
+root = tk.Tk()
+root.title("Text to Audio Converter")
+
+# Create a text entry field
+text_entry = tk.Entry(root, width=40)
+text_entry.pack(pady=20)
+
+# Create a button to trigger text-to-audio conversion
+convert_button = tk.Button(root, text="Convert to Audio", command=convert_to_audio)
+convert_button.pack()
+
+# Create a button to play the audio
+play_button = tk.Button(root, text="Play Audio", command=play_audio)
+play_button.pack()
+
+# Run the GUI application
+root.mainloop()
